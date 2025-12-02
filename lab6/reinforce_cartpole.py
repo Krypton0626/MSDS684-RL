98b0242e289877b00a27bed3c5ead1e239738f39
Uttam Emmaniuel Dammu <mannygarrix@gmail.com>
2025-12-02 14:16:48 -0700

    lab6: implement REINFORCE with baseline single-seed version

--- lab6/reinforce_cartpole.py
+++ lab6/reinforce_cartpole.py
@@ -88,6 +88,42 @@ def train_reinforce_single_seed(
     return np.array(episode_returns, dtype=np.float32)
 
 
+def run_episode_reinforce_baseline(env, policy, value_net, device: torch.device):
+    """
+    Run one full episode using policy + value baseline.
+
+    Returns:
+        states:    list of state tensors (on device)
+        log_probs: list of log π(a|s)
+        rewards:   list of rewards
+    """
+    state, _ = env.reset()
+    done = False
+
+    states = []
+    log_probs = []
+    rewards = []
+
+    while not done:
+        state_tensor = torch.from_numpy(state).float().to(device)
+
+        logits = policy(state_tensor)
+        dist = Categorical(logits=logits)
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+
+        next_state, reward, terminated, truncated, _ = env.step(action.item())
+        done = terminated or truncated
+
+        states.append(state_tensor)
+        log_probs.append(log_prob)
+        rewards.append(reward)
+
+        state = next_state
+
+    return states, log_probs, rewards
+
+
 def train_reinforce_baseline_single_seed(
     num_episodes: int = 500,
     gamma: float = 0.99,
@@ -96,5 +132,70 @@ def train_reinforce_baseline_single_seed(
     seed: int = 0,
     device: str = "cpu",
 ) -> np.ndarray:
-    # # TODO baseline version will use: advantages = returns_t - values.detach()
-    raise NotImplementedError
+    """
+    REINFORCE with a learned state-value baseline V(s).
+
+    Actor update uses advantage:
+        A_t = G_t - V(s_t)
+
+    Critic is trained with MSE to fit Monte Carlo returns.
+    """
+    device = torch.device(device)
+
+    # Reproducibility
+    set_global_seed(seed)
+
+    # Environment
+    env = make_cartpole_env(seed)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+
+    # Networks
+    policy = PolicyNetwork(state_dim, action_dim).to(device)
+    value_net = ValueNetwork(state_dim).to(device)
+
+    policy_opt = optim.Adam(policy.parameters(), lr=lr_policy)
+    value_opt = optim.Adam(value_net.parameters(), lr=lr_value)
+
+    episode_returns = []
+
+    for episode in range(num_episodes):
+        # Generate one episode
+        states, log_probs, rewards = run_episode_reinforce_baseline(
+            env, policy, value_net, device
+        )
+
+        # Monte Carlo returns
+        returns = compute_returns(rewards, gamma)            # (T,)
+        returns_t = torch.from_numpy(returns).to(device)
+
+        # Stack states and get values
+        state_batch = torch.stack(states)                    # (T, state_dim)
+        values = value_net(state_batch)                      # (T,)
+
+        # Advantage A_t = G_t - V(s_t), but detach baseline for actor
+        advantages = returns_t - values.detach()
+
+        # Policy loss
+        policy_losses = []
+        for log_prob, A_t in zip(log_probs, advantages):
+            policy_losses.append(-log_prob * A_t)
+        policy_loss = torch.stack(policy_losses).sum()
+
+        # Value loss (MSE between returns and V(s))
+        value_loss = torch.nn.functional.mse_loss(values, returns_t)
+
+        # Update actor
+        policy_opt.zero_grad()
+        policy_loss.backward()
+        policy_opt.step()
+
+        # Update critic
+        value_opt.zero_grad()
+        value_loss.backward()
+        value_opt.step()
+
+        episode_returns.append(sum(rewards))
+
+    env.close()
+    return np.array(episode_returns, dtype=np.float32)
